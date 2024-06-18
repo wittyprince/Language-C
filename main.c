2cc2c920e9187d6ddfd4077fe8f3a15213df1c98
wangchen <516760573@qq.com>
2024-06-18 22:17:34 +0800

    C-scanf使用

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #define PI2 (3 + 2)
 
 int main() { // main()入口函数
-    int i = 13;
+    int i = 123;
     printf("Hello, World!\n"); // 函数内的代码内容
 
     printf("%d\n", PI1 * 2); // 7, not 10, 注意这里的结果
@@ -33,6 +33,13 @@ int main() { // main()入口函数
     printf("%5s\n", s1); //  abc,右对齐, abc前面有两个空格
     printf("%5s\n", s2); // abcdef, 并没有被截掉为5个字符
 
+    int ii;
+    scanf("%d", &ii); // 注意一定要取地址 &ii
+    printf("ii=:%d\n", ii);
+//    fflush(stdin); // 清空标准输入缓冲区
+    char cc;
+    scanf("%c", &cc);
+    printf("%c\n", cc);
 
     return 0; // 函数的返回值
 }
